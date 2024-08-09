4d716b99bc4e16f763ec621589028f8abbc64d98
jain-dharamshaala <jain-dharamshaala@gmail.com>
2024-08-09 20:30:13 +0530

    test-project

---
+++ app.js
@@ -0,0 +1,18 @@
+// Filename: app.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/greet/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}!`);
+});
+
+app.get('/age/:name', (req, res) => {
+  // For simplicity, let's assume everyone is 25.
+  res.send(`${req.params.name} is 25 years old.`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
